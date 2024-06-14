8877f4dbcd46aa4e903046b1ed619c4ec8144206
Berlin <evan1234.ek@gmail.com>
2024-06-14 15:57:01 +0600

    card wrapper complete for auth forms

---
+++ components/auth/card-wrapper.tsx
@@ -0,0 +1,42 @@
+'use client';
+
+import React from 'react';
+
+import { BackButton } from '@/components/auth/back-button';
+import { Header } from '@/components/auth/header';
+import { Social } from '@/components/auth/social';
+
+import { Card, CardContent, CardFooter, CardHeader } from '../ui/card';
+
+interface CardWrapperProps {
+  children: React.ReactNode;
+  headerLabel: string;
+  backButtonLabel: string;
+  backButtonHref: string;
+  showSocial?: boolean;
+}
+
+export const CardWrapper = ({
+  children,
+  headerLabel,
+  backButtonLabel,
+  backButtonHref,
+  showSocial,
+}: CardWrapperProps) => {
+  return (
+    <Card className="w-[400px] shadow-md">
+      <CardHeader>
+        <Header label={headerLabel} />
+      </CardHeader>
+      <CardContent>{children}</CardContent>
+      {showSocial && (
+        <CardFooter>
+          <Social />
+        </CardFooter>
+      )}
+      <CardFooter>
+        <BackButton href={backButtonHref} label={backButtonLabel} />
+      </CardFooter>
+    </Card>
+  );
+};
---
+++ components/auth/social.tsx
@@ -0,0 +1,20 @@
+'use client';
+
+import React from 'react';
+import { FaGithub } from 'react-icons/fa';
+import { FcGoogle } from 'react-icons/fc';
+
+import { Button } from '../ui/button';
+
+export const Social = () => {
+  return (
+    <div className="flex items-center w-full gap-x-2">
+      <Button size="lg" className="w-full" variant="outline" onClick={() => {}}>
+        <FcGoogle className="h-6 w-6" />
+      </Button>
+      <Button size="lg" className="w-full" variant="outline" onClick={() => {}}>
+        <FaGithub className="h-6 w-6" />
+      </Button>
+    </div>
+  );
+};
