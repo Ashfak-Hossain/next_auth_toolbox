668e9e414a311be837a50dcce4f77fe180b5a9c4
Berlin <evan1234.ek@gmail.com>
2024-06-15 00:57:35 +0600

    implement next-auth, middleware, auth, credentials login

---
+++ routes.ts
@@ -0,0 +1,43 @@
+/**
+ * routes.js
+ *
+ * This module defines the public routes for the application.
+ *
+ * The public routes are the paths that can be accessed without authentication.
+ *
+ * This is useful for defining open areas of your web application, such as the home page, login page, etc.
+ */
+
+/**
+ * An array of public routes
+ *
+ * These routes are accessible without any authentication.
+ *
+ * @type {string[]}
+ */
+export const publicRoutes = ['/'];
+
+/**
+ * An array of routes that require authentication
+ *
+ * These routes are only accessible if the user is authenticated.
+ *
+ * @type {string[]}
+ */
+export const authRoutes = ['/auth/login', '/auth/register'];
+
+/**
+ * The prefix for API authentication routes
+ *
+ * Routes that start with this prefix are considered API authentication routes.
+ *
+ * @type {string}
+ */
+export const apiAuthPrefix = '/api/auth';
+
+/**
+ * The default redirect path after logging in
+ *
+ * @type {string}
+ */
+export const DEFAULT_LOGIN_REDIRECT = '/settings';
